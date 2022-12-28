d64ceea3d1f9d65a29e3563499487e5ed41e3ccf
Nekxis <nexispolska16@gmail.com>
2022-12-28 18:14:40 +0100

    nav

--- recipe-app/pages/index.tsx
+++ recipe-app/pages/index.tsx
@@ -1,10 +1,23 @@
-
+import { Flex, Center, Text, Spacer, Avatar } from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
 export default function Home() {
   return (
     <>
     <main>
-      Test
+      <Flex h='5rem'>
+        <Center w='7rem'>
+          <Text><Avatar /></Text>
+        </Center>
+        <Spacer />
+        <Center w='30rem'>
+          <Text color='green.400' fontSize='5xl' fontWeight='700'>Recipe app</Text>
+        </Center>
+        <Spacer />
+        <Center w='7rem'>
+          <Text fontSize='xl'><HamburgerIcon boxSize='8' /></Text>
+        </Center>
+      </Flex>
     </main>
     </>
   )
